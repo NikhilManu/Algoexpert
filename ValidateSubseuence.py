592ee4ed7267969d85239d7fff02669020235d59
NikhilManu <70922592+NikhilManu@users.noreply.github.com>
2020-10-29 21:41:05 +0530

    Update ValidateSubseuence.py

--- ValidateSubseuence.py
+++ ValidateSubseuence.py
@@ -1,1 +1,20 @@
+# Time - O(n)   Space - O(1)
+def validate(nums,seq):
+    if len(seq) > len(nums):
+        return False
+    
+    if nums == seq:
+        return True
+    
+    ind = 0
+    for x in nums:
+        if len(seq) == ind:
+            return True
+        if x == seq[ind]:
+            ind += 1
+    return False
 
+
+
+# we dont need to continue if length of seq array is greater than the input array
+# we can also check if input array and the sequence array are equal
