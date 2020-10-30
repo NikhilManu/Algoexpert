4b1a863e8a6c3b835caa42a2fcba9de8ec48b291
NikhilManu <70922592+NikhilManu@users.noreply.github.com>
2020-10-30 21:47:02 +0530

    Update 6. Monotonic Array.py

--- Arrays/6. Monotonic Array.py
+++ Arrays/6. Monotonic Array.py
@@ -1,1 +1,28 @@
+"""
+An array is monotonic if it is either monotone increasing or monotone decreasing.
 
+An array A is monotone increasing if for all i <= j, A[i] <= A[j].  An array A is monotone decreasing if for all i <= j, A[i] >= A[j].
+Return true if and only if the given array A is monotonic.
+
+Input: [1,2,2,3]
+Output: true
+
+Input: [1,3,2]
+Output: false
+"""
+
+# Time - O(n) |  Space - O(1)
+def isMonotonic(nums):
+    if len(nums) <= 2:
+        return True
+    
+    direction = nums[1] - nums[0]
+    for i in range(2,len(nums)):
+        if direction == 0:
+            direction = nums[i] - nums[i-1]
+        else:
+            current_direction = nums[i] - nums[i-1]
+            if (direction > 0 and current_direction < 0) or (direction < 0 and current_direction > 0):
+                return False
+    return True
+   
