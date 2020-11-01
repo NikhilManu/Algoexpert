1fa1d6abf60dcaf51b3a7c7197609d7474731e86
NikhilManu <70922592+NikhilManu@users.noreply.github.com>
2020-11-01 22:37:34 +0530

    Update 9. 4Sum.py

--- Arrays/9. 4Sum.py
+++ Arrays/9. 4Sum.py
@@ -1,3 +1,4 @@
+# This is a Long Post. The 2nd qn used is from leetcode.
 """
 Given an array nums of n integers and an integer target, are there elements a, b, c, and d in nums such that a + b + c + d = target? 
 Find all unique quadruplets in the array which gives the sum of target.
