5669c51ebbb81ac49076a4982a7f26344db34ef9
NikhilManu <70922592+NikhilManu@users.noreply.github.com>
2020-11-02 21:15:03 +0530

    Update 12. Min Rewards.py

--- Arrays/12. Min Rewards.py
+++ Arrays/12. Min Rewards.py
@@ -36,6 +36,45 @@ def MinRewards(nums):
 # Solution 2 ----> Peak - Valley or Local min - Local Max Solution
 # Time - O(n)  | Space - O(n)
 def MinRewards(nums):
+    res = [1] * len(nums)
+    local_mins = localmin(nums)
+    for i in local_mins:
+        res = expand(i,nums,res)
+    return sum(res)
+
+def localmin(arr):
+    if len(arr) == 1:
+        return [0]
+    local_mins =[]
+    for i in range(len(arr)):
+        if i == 0 and arr[i] < arr[i+1]:
+            local_mins.append(i)
+        if i == len(arr) - 1 and arr[i] < arr[i-1]:
+            local_mins.append(i)
+        if i == 0 or i == len(arr) - 1:
+            continue
+        if arr[i-1] > arr[i] < arr[i+1]:
+            local_mins.append(i)
+
+    return local_mins
+
+def expand(i,nums,res):
+    l = i - 1
+    while l >= 0 and nums[l] > nums[l+1]:
+        res[l] = max(res[l],res[l+1] + 1)
+        l -= 1
+    r = i + 1
+    while r < len(nums) and nums[r] > nums[r-1]:
+        res[r] = res[r-1] + 1
+        r += 1
+        
+    return res
+
+
+# Solution 3 ---> Left - Right Pass.
+# Time  - O(n)  |  Space -  O(n)
+def MinRewards(nums):
+    
     
                 
     
