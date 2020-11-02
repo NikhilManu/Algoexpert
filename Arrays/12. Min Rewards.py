f08ce44621b22fca47f0c9adb559bb545f59ad4c
NikhilManu <70922592+NikhilManu@users.noreply.github.com>
2020-11-02 20:16:23 +0530

    Update 12. Min Rewards.py

--- Arrays/12. Min Rewards.py
+++ Arrays/12. Min Rewards.py
@@ -18,5 +18,27 @@ Input : [8,4,2,1,3,6,7,9,5]
 Output : 25
 
 """
+# Solution 1 ---> Naive Solution
+# Time - O(n^2)  |  Space - O(n)
+def MinRewards(nums):
+    res = [1] * len(nums)
+    for i in range(1,len(nums)):
+        j = i - 1
+        if nums[i] > nums[j]:
+            res[i] = res[j] + 1
+        else:
+            while j >= 0 and nums[j] > nums[j+1]:
+                res[j] = max(res[j] , res[j+1] + 1)
+                j -= 1
+    return sum(res)
+
+
+# Solution 2 ----> Peak - Valley or Local min - Local Max Solution
+# Time - O(n)  | Space - O(n)
+def MinRewards(nums):
+    
+                
+    
+  
 
 
