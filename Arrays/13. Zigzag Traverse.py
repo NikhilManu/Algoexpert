59925d76564f3d574a72e0e08afba4cc2d907bd0
NikhilManu <70922592+NikhilManu@users.noreply.github.com>
2020-11-03 03:23:27 +0530

    Update 13. Zigzag Traverse.py

--- Arrays/13. Zigzag Traverse.py
+++ Arrays/13. Zigzag Traverse.py
@@ -1,1 +1,107 @@
+"""
+-------------------
+Diagonal Traverse [Name of this Qn in LeetCode]
+------------------
+"""
+"""
+Type 1 of ZigZag.
+
+Input:
+[
+ [ 1, 2, 3 ],
+ [ 4, 5, 6 ],
+ [ 7, 8, 9 ]
+]
+
+Output:  [1,4,2,3,5,7,8,6,9]
+
+"""
+# Time - O(n)  |  Space - O(n)
+def ZigZag(arr):
+    if not arr:
+        return []
+    res = []
+    tr = len(m) - 1
+    tc = len(m[0]) - 1
+    r,c = 0,0
+    down = True
+    while r >= 0 and r <= tr and c >= 0 and c <= tc:
+        res.append(arr[r][c])
+        if down:
+            if c == 0 or r == tr:
+                down = False
+                if r == tr:
+                    c += 1
+                else:
+                    r += 1
+            else:
+                r += 1
+                c -= 1
+        else:
+            if r == 0 or c == tc:
+                down = True
+                if c == tc:
+                    r += 1
+                else:
+                    c += 1
+            else:
+                r -= 1
+                c += 1
+
+
+    return res
+
+
+
+"""
+Type 2 of ZigZag.
+
+Input:
+[
+ [ 1, 2, 3 ],
+ [ 4, 5, 6 ],
+ [ 7, 8, 9 ]
+]
+
+Output:  [1,2,4,7,5,3,6,8,9]
+
+"""
+# Time - O(n)  |  Space - O(n)
+def ZigZag(arr):
+    if not arr:
+        return []
+    res = []
+    tr = len(m) - 1
+    tc = len(m[0]) - 1
+    r,c = 0,0
+    down = False    # This is the only change as we have to go up first time.
+    while r >= 0 and r <= tr and c >= 0 and c <= tc:
+        res.append(arr[r][c])
+        if down:
+            if c == 0 or r == tr:
+                down = False
+                if r == tr:
+                    c += 1
+                else:
+                    r += 1
+            else:
+                r += 1
+                c -= 1
+        else:
+            if r == 0 or c == tc:
+                down = True
+                if c == tc:
+                    r += 1
+                else:
+                    c += 1
+            else:
+                r -= 1
+                c += 1
+
+
+    return res
+
+
+
+  
 
