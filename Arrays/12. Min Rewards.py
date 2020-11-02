8b9c107f3c024cea1dfa35cd0aee40349125483e
NikhilManu <70922592+NikhilManu@users.noreply.github.com>
2020-11-02 19:16:42 +0530

    Create 12. Min Rewards.py

---
+++ Arrays/12. Min Rewards.py
@@ -0,0 +1,21 @@
+"""
+-------------
+Candy  [Name of this Qn in LeetCode]
+-------------
+"""
+
+"""
+Given a list of scores of students distribute them rewards such that
+- all students must receive at least 1 reward
+- any student must receive more rewards than any adjacent student with lower score & vice versa
+
+Note: Each Score is Unique.
+
+Return the minimum number of rewards you must give to the students
+
+EXAMPLE:
+[8,4,2,1,3,6,7,9,5]  ->  [4,3,2,1,2,3,4,5,1]  ->  25
+
+"""
+
+
