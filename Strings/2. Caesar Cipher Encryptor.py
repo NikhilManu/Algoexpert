2871606a371019c4208761c59f35ef4ecfb871cf
NikhilManu <70922592+NikhilManu@users.noreply.github.com>
2020-11-03 21:57:56 +0530

    Update 2. Caesar Cipher Encryptor.py

--- Strings/2. Caesar Cipher Encryptor.py
+++ Strings/2. Caesar Cipher Encryptor.py
@@ -1,1 +1,11 @@
+"""
+Caesar Cipher Encryptor
+Given a non-empty string of lowercase letters and a non-negative integer representing a key, write a function
+that returns a new string obtained by shifting every letter in the input string by k positions in the alphabet,
+where k is the key.
+Note that letters should "wrap" around the alphabet;
+in other words, the letter z shifted by one returns the  letter a.
 
+Sample Input: "xyz", 2
+Sample Output: "zab"
+"""
