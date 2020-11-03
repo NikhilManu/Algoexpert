4e9d452d6171535120d3953fb6c12a11f01d510a
NikhilManu <70922592+NikhilManu@users.noreply.github.com>
2020-11-03 22:42:19 +0530

    Update 2. Caesar Cipher Encryptor.py

--- Strings/2. Caesar Cipher Encryptor.py
+++ Strings/2. Caesar Cipher Encryptor.py
@@ -9,3 +9,40 @@ in other words, the letter z shifted by one returns the  letter a.
 Sample Input: "xyz", 2
 Sample Output: "zab"
 """
+
+# Solution 1 --->
+# Time - O(n)  | Space - O(n)
+------------------------
+def CaesarCipher(s,key):
+    res = []
+    key = key % 26
+    for i in s:
+        unicode = ord(i) + key
+        if unicode <= 122:
+            res.append(chr(unicode))
+        else:
+            res.append(chr(96 + unicode % 122 ))
+            
+    return "".join(res)
+
+
+# Solution 2 --->
+# Time - O(n)  | Space - O(n)
+------------------------
+def CaesarCipher(s,key):
+    res = []
+    key = key % 26 
+    alpha = list("abcdefghijklmnopqrstuvwxyz")
+    for i in s:
+        newletter = alpha.index(i) + key
+        if newletter <= 25:
+            res.append(alpha[newletter])
+        else:
+            res.append(alpha[-1 + newletter % 25])
+            
+    return res
+
+
+    
+        
+  
