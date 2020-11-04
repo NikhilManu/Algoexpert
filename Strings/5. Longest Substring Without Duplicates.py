2e9d94b23b42400262c1c180538ba098a2649692
NikhilManu <70922592+NikhilManu@users.noreply.github.com>
2020-11-04 17:23:56 +0530

    Create 5. Longest Substring Without Duplicates.py

---
+++ Strings/5. Longest Substring Without Duplicates.py
@@ -0,0 +1,12 @@
+"""
+--------------
+Longest Substring Without Repeating Characters [Name of this Qn in LeetCode]
+--------------
+"""
+"""
+Given a string s, find the length of the longest substring without repeating characters.
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+"""
