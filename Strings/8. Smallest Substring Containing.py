464976f79de2b9ef8b825f04efbf11d6dcd7b505
NikhilManu <70922592+NikhilManu@users.noreply.github.com>
2020-11-06 16:49:51 +0530

    Update 8. Smallest Substring Containing.py

--- Strings/8. Smallest Substring Containing.py
+++ Strings/8. Smallest Substring Containing.py
@@ -17,6 +17,64 @@ Output: "BANC"
 
 # Time - O()  | Space - O()
 --------------------------
+from collections import Counter
+
 def SmallestSubstringContains(s,t):
-    if not s or not t:  # if s or t is empty
+    if not s or not t or len(s) < len(t):  # if s or t is empty or if length of s is less than that of t.
         return '' 
+    
+    if len(s) == len(t):
+        if s == t:
+            return s
+        else:
+            return ''
+    
+    bound = [0,float('inf')]
+    target = Counter(t)     # Ask to interviewer if you can use dependencies, if he says no implement it yourself like in Algoexpert
+    current = {}
+    targetlen = len(target)
+    currentlen = 0
+    
+    l,r = 0,0
+    while r < len(s):
+        if s[r] not in target: 
+            r += 1
+            continue
+            
+        increaseCount(s[r],current)
+        if current[s[r]] == target[s[r]]:
+            currentlen += 1
+        
+        while currentlen == targetlen and l <= r:
+            bound = [l,r] if r - l < bound[1] - bound[0] else bound
+            
+            if s[l] not in target:
+                l += 1
+                continue
+            
+            if current[s[l]] == target[s[l]]:
+                currentlen -= 1
+            decreaseCount(s[l],current)
+            l += 1
+        
+        r += 1
+        
+    return getStringfromBound(s,bound)
+
+
+def getStringfromBound(s,bound):
+    if bound[1] == float('inf'):
+        return ""
+    return s[bound[0]: bound[1] + 1]
+                         
+
+def decreaseCount(char, current):
+    current[char] -= 1
+
+def increaseCount(char, current):
+    if char in current:
+        current[char] += 1
+    current[char] = 1
+    
+    
+    
