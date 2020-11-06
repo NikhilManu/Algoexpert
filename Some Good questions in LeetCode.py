acc1a97b3ae4b1430865aa0b75278eca55914201
NikhilManu <70922592+NikhilManu@users.noreply.github.com>
2020-11-06 21:21:25 +0530

    Create Some Good questions in LeetCode.py

---
+++ Some Good questions in LeetCode.py
@@ -0,0 +1,5 @@
+These Qns are in no Order. These are qns which I think are good for interview Preparation.
+All these Qns are available in LeetCode.
+
+1. Minimum Absolute Difference 
+2. Valid Parentheses
