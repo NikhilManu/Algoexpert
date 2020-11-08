8d8c3a437b8879e6e3379422db632b2cb6c055d4
NikhilManu <70922592+NikhilManu@users.noreply.github.com>
2020-11-09 03:45:10 +0530

    Update 3. Find Loop.py

--- LinkedList/3. Find Loop.py
+++ LinkedList/3. Find Loop.py
@@ -7,8 +7,9 @@ Linked List Cycle II [ Name of this Qn in LeetCode ]
 Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
 
 """
+# This Solution is more comfortable for me. Since i dont have to worry about initialising fast and slow with head.next.next and head.next
 
-# 
+# O(N) Time | O(1) Space
 ------------
 def FindLoop(node):
     fast = slow = head
