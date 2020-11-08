fb112291d1de2b97ff46a4bad6f9f8a883223a60
NikhilManu <70922592+NikhilManu@users.noreply.github.com>
2020-11-09 03:43:38 +0530

    Update 3. Find Loop.py

--- LinkedList/3. Find Loop.py
+++ LinkedList/3. Find Loop.py
@@ -1,13 +1,27 @@
 """
 -------------------
-Linked List Cycle [ Name of this Qn in LeetCode ]
+Linked List Cycle II [ Name of this Qn in LeetCode ]
 ------------------
 """
 """
-Given head, the head of a linked list, determine if the linked list has a cycle in it.
+Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
 
 """
 
 # 
 ------------
 def FindLoop(node):
+    fast = slow = head
+    while fast and fast.next:
+        fast = fast.next.next
+        slow = slow.next
+        if fast == slow:
+            fast = head
+            while fast != slow:
+                fast = fast.next
+                slow = slow.next
+                
+            return slow
+        
+    return None
+  
