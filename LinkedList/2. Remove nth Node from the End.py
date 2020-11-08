7660201cc614b52348af360658a834d1102d4e64
NikhilManu <70922592+NikhilManu@users.noreply.github.com>
2020-11-09 03:06:26 +0530

    Create 2. Remove nth Node from the End.py

---
+++ LinkedList/2. Remove nth Node from the End.py
@@ -0,0 +1,19 @@
+"""
+-----------
+Remove Nth Node From End of List [ Name of the Qn in LeetCode ]
+-----------
+"""
+"""
+Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+Input: head = [1,2,3,4,5], n = 2 
+Output: [1,2,3,5]
+
+Input is not an array. It is only a representation of the linked List
+"""
+
+# 
+---------------
+def NthnodeFromEnd(node,n):
+ 
+  
