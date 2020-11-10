4efafdfd674c34e3e3abca3bdbba1e73ec81bcc9
NikhilManu <70922592+NikhilManu@users.noreply.github.com>
2020-11-10 13:20:17 +0530

    Create 6. Shift Linkedlist.py

---
+++ LinkedList/6. Shift Linkedlist.py
@@ -0,0 +1,14 @@
+"""
+---------------
+Rotate List [ Name of this Qn in Leetcode ]
+--------------
+"""
+"""
+Given a head of linked list and integer k, Rotate the linkedlist by k to the right if k is +ve.
+Rotate to the left by k places if k is -ve.
+"""
+
+# Solution 1 --->
+# O(N) Time | O(1) Space
+-------------------
+def ShiftLinkedlist(head,k):
