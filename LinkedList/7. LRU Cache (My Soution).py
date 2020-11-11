ae98a0d62a5fcf4a0fafde3facd1fbcb8dcf8ef6
NikhilManu <70922592+NikhilManu@users.noreply.github.com>
2020-11-11 17:54:12 +0530

    Update 7. LRU Cache (My Soution).py

--- LinkedList/7. LRU Cache (My Soution).py
+++ LinkedList/7. LRU Cache (My Soution).py
@@ -19,14 +19,14 @@ class LRUCache:
         self.currentSize = 0
         self.cache = {}
         self.head = Node('#','#')   # We create sentinel head  --> Look Below to know what is a sentinel node
-        self.tail = Node('#','#')   # We create sentinel tail
+        self.tail = Node('#','#')   # We create sentinel tail  --> Look Below to know what is a sentinel node
         self.head.next = self.tail
         self.tail.prev = self.head
         
     def put(self, key, value):
         if key in self.cache:
             self.remove(self.cache[key])
-            self.cache[key].value = value   # Since key already exist we have to only change the value
+            self.cache[key].value = value   # Since key already exist in cache we have to only change the value
         else:
             if self.currenSize == self.maxsize:
                 LRU = self.tail.prev.key
@@ -44,3 +44,23 @@ class LRUCache:
         self.remove(self.cache[key])
         self.add(self.cache[key])
         return self.cache[key].value
+    
+    def remove(self,node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+        node.next = None
+        node.prev = None
+        
+    def add(self,node):
+        node.prev = self.head
+        node.next = self.head.next
+        node.prev.next = node
+        node.next.prev = node
+        
+        
+"""
+Sentinal Node: Sentinal nodes next pointer gives the head of the linked list
+
+So here, sentinal head's next pointer gives head of the linked list 
+Similarly, sentinal tail's prev pointer gives the tail of the linked list
+"""
