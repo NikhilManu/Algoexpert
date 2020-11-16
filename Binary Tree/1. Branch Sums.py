607c7323a67e8a147f41507696153202fbc8eeaa
NikhilManu <70922592+NikhilManu@users.noreply.github.com>
2020-11-16 17:14:22 +0530

    Update 1. Branch Sums.py

--- Binary Tree/1. Branch Sums.py
+++ Binary Tree/1. Branch Sums.py
@@ -1,1 +1,42 @@
+"""
+Path Sum 
+            ---> [ Name of the Qn in LeetCode ]
+Path Sum II
+"""
 
+"""
+Given a Binary Tree, Compute Sum of the Branches and return them in an array
+
+Input: 
+
+      5
+     / \
+    4   8
+   /   / \
+  11  13  4
+ /  \      \
+7    2      1
+
+
+output : [27, 22, 26, 18]
+"""
+# Solution --->
+# O(N) Time and Space
+---------------
+def BranchSums(root):
+    sums = []
+    calculateSum(root, currentSum, sums)
+    return sums
+
+def calculateSum(node, currentSum, sums):
+    if not node:
+        return
+    
+    currentSum += node.val
+    if not node.left and not node.right:
+        sums.append(currentSum)
+        return 
+    
+    calculateSum(node.left, currentSum, sums)
+    calculateSum(node.right, currentSum, sums)
+    
