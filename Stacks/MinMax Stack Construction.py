43aa40ce0b6cd3ac87a16867ecf23e6cfbbeb526
NikhilManu <70922592+NikhilManu@users.noreply.github.com>
2020-11-16 14:59:25 +0530

    Update MinMax Stack Construction.py

--- Stacks/MinMax Stack Construction.py
+++ Stacks/MinMax Stack Construction.py
@@ -1,1 +1,41 @@
+"""
+Min Stack 
+          ---[Name of these Qns in Leetcode]
+Max Stack
+"""
+"""
 
+Construct a Stack which has function 
+1. push
+2. pop
+3. peek or Top
+4. min
+5. max
+
+All these Operations should be in O(1) Time 
+
+"""
+# Solution ---> AlgoExpert Solution
+# O(1) Time and Space for all Funtion
+---------------------------------------
+class MinMaxStack:
+    def __init__(self):
+        pass
+    
+    def push(self, val):
+        pass
+    
+    def pop(self):
+        pass
+    
+    def peek(self):
+        pass
+    
+    def getMin(self):
+        pass
+    
+    def getMax(self):
+        pass
+    
+      
+  
