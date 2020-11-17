9163febe5abf7f248b25c794eefed6f13074bb0e
NikhilManu <70922592+NikhilManu@users.noreply.github.com>
2020-11-18 03:07:28 +0530

    Update Iterative Inorder Traversal.py

--- Binary Tree/Iterative Inorder Traversal.py
+++ Binary Tree/Iterative Inorder Traversal.py
@@ -2,10 +2,46 @@
  Binary Tree Inorder Traversal [ Similar Qn in LeetCOde ]
 
 """
+#AlgoExpert Solution for is BullShit. just Learn Morris Traversal
 
-
-# Solution ----
-# Time O(N) | O(1) Space
+# Solution ----> With stack
+# Time O(N) | O(N) Space
 --------------------------
-def InorderTraversal(root):
-  
+def InorderTraversal(root, callback):
+    stack = []
+    cur = root
+    while cur or stack:
+        while cur:
+            stack.append(cur)
+            cur = cur.left
+    
+        if stack:
+            node = stack.pop()
+            callback(node)
+            cur = node.right
+            
+    
+ # Solution ----> Morris Traversal
+# Time O(N) | O(N) Space
+-------------------------- 
+def InorderTraversal(root, callback):
+    current = root
+    predecessor = None  
+    while current:
+        if not current.left:
+            callback(current)
+            current = current.right
+        else:
+            predecessor = current.left
+            whlie predecessor.right and predecessor.right != current:   # Finding Inorder Predecessor 
+                predecessor = predecessor.right
+                
+            if not predecessor.right:
+                predecessor.right = cur # Link predecessor and cur Node
+                cur = cur.left
+            else:
+                callback(current)
+                current = current.right
+                predecessor.right = None
+                
+                
