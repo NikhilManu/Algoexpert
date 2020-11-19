78d14921bc58f339d0b49f1afb0b2bc440bcbfff
NikhilManu <70922592+NikhilManu@users.noreply.github.com>
2020-11-19 23:01:53 +0530

    Update Right Sibling Tree.py

--- Binary Tree/Right Sibling Tree.py
+++ Binary Tree/Right Sibling Tree.py
@@ -5,3 +5,34 @@ Populating Next Right Pointers in Each Node
 Populating Next Right Pointers in Each Node ll
 """
 
+"""
+
+"""
+
+# Solution --
+# O(N) Time | O(1) Space
+--------------------------
+def RightSiblingTree(root):
+    mutate(root, None, None)
+    return root
+
+def mutate(node, parent, isleftChild):
+    if not node:
+        return 
+    
+    left, right = node.left, node.right
+    mutate(left, node, True)
+    
+    if not parent:
+        node.right = None
+    elif isleftChild:
+        node.right = parent.right
+    else:
+        if not parent.right:
+            node.right = None
+        else:
+            node.right = parent.right.left
+        
+    mutate(right, node, False)
+    
+    
