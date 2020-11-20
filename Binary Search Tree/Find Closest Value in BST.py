ba4e82a51da58e9ee11641b57047ee8c561a9b31
NikhilManu <70922592+NikhilManu@users.noreply.github.com>
2020-11-20 21:30:56 +0530

    Update Find Closest Value in BST.py

--- Binary Search Tree/Find Closest Value in BST.py
+++ Binary Search Tree/Find Closest Value in BST.py
@@ -1,1 +1,13 @@
+"""
+QN in Leetcode is for premium Members Only
+"""
+
+"""
+Given the root of BST and a Target Value, We Have to find a value in BST such That it is the closest value to the target Value
+
+"""
+
+# Solution ---
+# Time O(N) | Space O(1)
+----------------------
 
