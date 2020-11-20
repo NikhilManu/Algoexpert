4694e0d46fd066f238db4b52749aa55181d75f24
NikhilManu <70922592+NikhilManu@users.noreply.github.com>
2020-11-20 21:47:47 +0530

    Update Find Closest Value in BST.py

--- Binary Search Tree/Find Closest Value in BST.py
+++ Binary Search Tree/Find Closest Value in BST.py
@@ -7,7 +7,48 @@ Given the root of BST and a Target Value, We Have to find a value in BST such Th
 
 """
 
-# Solution ---
-# Time O(N) | Space O(1)
+# Solution --- Recursive
+# Time O(log N) | Space O(log N)     ----> Worst Case O(N) Time and Space
 ----------------------
+def FindClosestValue(root, target):
+    return Helper(root, target, float('inf'))
+
+def Helper(node, target, closestValue):
+    if not node:
+        return closestValue
+    
+    if abs(target - closestValue) > abs(target - node.val):
+        closestValue = node.val
+    
+    if target < node.val:
+        return Helper(node.left, target, closestValue)
+    elif target > node.val:
+        return Helper(node.right, target, closestValue)
+    else:
+        return closestValue
+    
+    
+# Solution --- Iterative
+# Time O(log N) | O(1) Space
+---------------------------------
+def FindClosestValue(root, target):
+    cur = root
+    closest = float('inf')
+    
+    while cur:
+        if abs(closest - target) > abs(target - cur.val):
+            closest = cur.val
+        
+        if target > cur.val:
+            cur = cur.right
+        elif target < cur.val:
+            cur = cur.left
+        else:
+            break
+            
+    return closest
+        
+    
+    
+  
 
