9eb86d4b41f24d0c8b6d3e5742059f2c43e798bd
NikhilManu <70922592+NikhilManu@users.noreply.github.com>
2020-11-23 14:12:08 +0530

    Update Validate BST.py

--- Binary Search Tree/Validate BST.py
+++ Binary Search Tree/Validate BST.py
@@ -1,1 +1,31 @@
+"""
+Validate Binary Search Tree [ Name of this Qn in LeetCode ]
+"""
+
+# Below AlgoExpert Solution, I have also provided answer to LeetCode Qn
+
+"""
+Given a BST, return True if it is a Valid BST
+
+Input:      5
+           / \
+          1   4
+             / \
+            3   6
+            
+Output: False
+
+"""
+
+# SOlution --
+# O(N) Time | O(d) Space
+----------------------
+def validBST(root):
+    return validateBST(root, float('-inf'), float('inf'))
+
+def validateBST(node, minVal, maxVal):
+    if not node:
+        return True
+   
+    if minVal > node.val
 
