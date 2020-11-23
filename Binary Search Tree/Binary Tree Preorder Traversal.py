67ac912c8e2d6600f509257cf3a1bdaa989dc686
NikhilManu <70922592+NikhilManu@users.noreply.github.com>
2020-11-23 22:38:20 +0530

    Update Binary Tree Preorder Traversal.py

--- Binary Search Tree/Binary Tree Preorder Traversal.py
+++ Binary Search Tree/Binary Tree Preorder Traversal.py
@@ -1,1 +1,43 @@
+"""
+Same Name in LeetCode
+"""
 
+"""
+Given root of BST, return the nodes in preorder Traversal
+
+Could You do it Iteratively
+"""
+
+# Solution 1 --- Recursive 
+# Time O(N)  | O(N) Space
+-------------------------
+def PreOrderTraversal(root):
+    return preorder(root, [])
+
+def preorder(node, res):
+    if not node:
+        return
+    
+    res.append(node.val)
+    preorder(node.left, res)
+    preorder(node.right, res)
+    
+    return res
+
+# Solution 2 --- Iteratively
+# Time O(N) | O(N) Space
+-----------------------------
+def PreorderTraversal(root):
+    res, stack = [], [root]
+    cur = root
+    while stack:
+        node = stack.pop()
+        if not node:
+            continue
+        
+        res.append(node.val)
+        stack.append(node.right)
+        stack.append(node.left)
+        
+    return res
+  
