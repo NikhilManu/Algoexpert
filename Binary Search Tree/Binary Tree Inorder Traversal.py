0c48ec5c8cf538199bfe4331c6c9f77c14e2d0e6
NikhilManu <70922592+NikhilManu@users.noreply.github.com>
2020-11-23 21:13:53 +0530

    Create Binary Tree Inorder Traversal.py

---
+++ Binary Search Tree/Binary Tree Inorder Traversal.py
@@ -0,0 +1,19 @@
+"""
+Same Name in LeetCode
+"""
+
+"""
+Given root of BST, return the inorder Traversal of the nodes 
+
+Recursion is trivial, 
+Follow Up:
+
+1. Do it Without Recursion
+2. Do it in O(1) Space
+
+"""
+
+# Solution 1 --- Recursive
+# O(N) Time | O(N) Space
+----------------------------
+
