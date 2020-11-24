9aba6659d47c5dba7df825f9d7a1c7ae1f492486
NikhilManu <70922592+NikhilManu@users.noreply.github.com>
2020-11-24 21:38:42 +0530

    Update 238. Product of Array Except Self.py

--- LeetCode/238. Product of Array Except Self.py
+++ LeetCode/238. Product of Array Except Self.py
@@ -1,1 +1,33 @@
+"""
+Given an array nums of n integers where n > 1,  return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+
+Example:
+
+Input:  [1,2,3,4]
+Output: [24,12,8,6]
+Constraint: It's guaranteed that the product of the elements of any prefix or suffix of the array (including the whole array) fits in a 32 bit integer.
+
+Note: Please solve it without division and in O(n).
+
+Follow up:
+Could you solve it with constant space complexity? (The output array does not count as extra space for the purpose of space complexity analysis.)
+
+"""
+# Easy Solution With division is not mentioned Here
+
+# Solution 1 --- DP Solution
+# Time O(N) | O(N) Space
+--------------------------
+def ProductExceptSelf(nums):
+    left = [1] * len(nums)
+    right = [1] * len(nums)
+    
+    for i in range(1, len(nums)):
+        left[i] = left[i-1] * nums[i-1]
+        
+    for i in reversed(range(len(nums)-1)):
+        right[i] = right[i+1] * nums[i+1]
+        
+    return [i*j for i,j in zip(left, right)]    # Same as multiplying left element and right element at same index
+
 
