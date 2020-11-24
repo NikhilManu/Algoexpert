4696d0c0b883c64d83e138b353bcd20e2c27c76c
NikhilManu <70922592+NikhilManu@users.noreply.github.com>
2020-11-24 15:05:14 +0530

    Update Binary Tree Postorder Traversal.py

--- Binary Search Tree/Binary Tree Postorder Traversal.py
+++ Binary Search Tree/Binary Tree Postorder Traversal.py
@@ -1,1 +1,46 @@
+"""
+Same name in Leetcode
+"""
+"""
+Given the root of a BST, return the nodes in postorder Traversal
 
+Could you Implement it iteratively
+"""
+
+# Solution 1 ---- Recursive
+# Time O(N) | O(N) Space
+------------------------
+def PostOrderTraversal(root):
+    return postorder(root, [])
+
+def postorder(node, res):
+    if not node:
+        return 
+    
+    postorder(node.left, res)
+    postorder(node.right, res)
+    res.append(node.val)
+    
+    return res
+
+
+# Solution 2 --- Iterative
+# Time O(N) | Space O(N)
+----------------------
+def PostOrder(root):
+    stack, res = [root], []
+    while stack:
+        node = stack.pop()
+        if not node:
+            continue
+        
+        res.append(node.val)
+        stack.append(node.left)
+        stack.append(node.right)
+        
+    return res[::-1]
+
+
+        
+      
+  
