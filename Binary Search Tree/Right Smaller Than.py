2cd16ec8f77b319e06117355ab57f2905bd75a20
NikhilManu <70922592+NikhilManu@users.noreply.github.com>
2020-11-26 22:39:56 +0530

    Create Right Smaller Than.py

---
+++ Binary Search Tree/Right Smaller Than.py
@@ -0,0 +1,27 @@
+"""
+ Count of Smaller Numbers After Self [ Name of this Qn in Leetcode ] ---> Not Sure if this is the Qn in AlgoExpert
+ 
+ PreRequisites: Binary Indexed Tree
+ Link For Tutorial: https://www.youtube.com/watch?v=CWDQJGaN1gY
+"""
+
+"""
+You are given an integer array nums and you have to return a new counts array. 
+The counts array has the property where counts[i] is the number of smaller elements to the right of nums[i].
+
+ 
+
+Example 1:
+
+Input: nums = [5,2,6,1]
+Output: [2,1,1,0]
+
+Explanation:
+To the right of 5 there are 2 smaller elements (2 and 1).
+To the right of 2 there is only 1 smaller element (1).
+To the right of 6 there is 1 smaller element (1).
+To the right of 1 there is 0 smaller element.
+"""
+def RightSmaller(nums):
+
+
