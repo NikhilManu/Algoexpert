1bbddf62c6442bcac3dbea6580e45f08e9192f11
NikhilManu <70922592+NikhilManu@users.noreply.github.com>
2020-11-28 22:46:31 +0530

    Update dfs.py

--- Graphs/dfs.py
+++ Graphs/dfs.py
@@ -1,1 +1,19 @@
+"""
+Dfs on A Graph
+"""
 
+# Time O(V+E) | Space O(V)
+--------------------
+class Node:
+    def __init__(self, name):
+        self.name = name 
+        self.children = []
+        
+    def addChildren(self, name):
+        self.children.append(Node(name))
+        
+    def DFS(self, arr):
+        array.append(self.name)
+        for child in self.children:
+            child.DFS(arr)
+        return arr
