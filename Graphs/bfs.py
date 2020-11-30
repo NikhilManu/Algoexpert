b35246f8c6500384075df9802434509f51ee7d29
NikhilManu <70922592+NikhilManu@users.noreply.github.com>
2020-11-30 14:03:06 +0530

    Update bfs.py

--- Graphs/bfs.py
+++ Graphs/bfs.py
@@ -4,5 +4,20 @@ implement BFS in a Graph
 
 # Time O(V+E) | O(V) Space
 --------------------
-
-
+class Node:
+    def __init__(self, name):
+        self.name = name 
+        self.children = []
+        
+    def addChild(self, name):
+        self.children.append(Node(name))
+        
+    def BFS(self, arr):
+        queue = deque([self])
+        while queue:
+            node = queue.popleft()
+            arr.append(node.name)
+            for child in node.children:
+                queue.append(child)
+                
+        return arr
