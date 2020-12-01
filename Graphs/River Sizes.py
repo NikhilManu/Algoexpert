3966cc9931101a21429948b48b124c3d388febb8
NikhilManu <70922592+NikhilManu@users.noreply.github.com>
2020-12-01 20:15:22 +0530

    Update River Sizes.py

--- Graphs/River Sizes.py
+++ Graphs/River Sizes.py
@@ -32,4 +32,33 @@ def outofBounds(i,j,grid):
     return i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0])
    
                        
-  
+        
+# Solution - Iterative Solution
+# Time O(R*C) | Space O(R*C)
+-------------------------
+def RiverSizes(grid):
+    area, seen = [], set()
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                area.append(DFS(grid, i, j, seen))
+                
+    return area
+
+def DFS(grid, i, j, seen):
+    stack = [(i,j)]
+    curArea = 0
+    while stack:
+        i,j = stack.pop()
+        if outofBounds(i, j, grid) or (i,j) in seen or grid[i][j] == 0:
+            continue
+        curArea += 1
+        stack.append((i+1, j))
+        stack.append((i-1, j))
+        stack.append((i, j+1))
+        stack.append((i, j-1))
+        
+    return area
+
+def outofBounds(i,j,grid):
+    return i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0])
