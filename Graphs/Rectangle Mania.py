7f2ef82f2ec1e2b1272ef5b6dd641c3d7b1d0027
NikhilManu <70922592+NikhilManu@users.noreply.github.com>
2020-12-03 21:49:15 +0530

    Update Rectangle Mania.py

--- Graphs/Rectangle Mania.py
+++ Graphs/Rectangle Mania.py
@@ -1,1 +1,14 @@
+"""
 
+"""
+"""
+Given list of co ordinates, Find the number of rectangle which can be formed by the given list of coordinates
+
+"""
+
+# Solution
+# Time O(N^2) | Space O(N^2)
+-------------------------------
+def RectangleMania(coords):
+    
+  
