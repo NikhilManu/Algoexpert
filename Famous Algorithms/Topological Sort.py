53e620d3b0c496830a0caac3b7679e1491052d6e
NikhilManu <70922592+NikhilManu@users.noreply.github.com>
2020-12-21 15:32:29 +0530

    Update Topological Sort.py

--- Famous Algorithms/Topological Sort.py
+++ Famous Algorithms/Topological Sort.py
@@ -11,5 +11,68 @@ If there are many valid answers, return any of them. If it is impossible to fini
 Input: numCourses = 4, prerequisites = [[1,0],[2,0],[3,1],[3,2]]
 Output: [3, 1, 2, 0]
 """
+def TopSort(jobs, deps):
+    jobgraph = createGraph(jobs, deps)
+    return getOrderedJob(jobgraph)
 
-52.39
+def getOrderedJob(jobgraph):
+    orderedJob = []
+    nodes = jobgraph.nodes
+    while nodes:
+        node = nodes.pop()
+        containsCycle = dfs(node, orderedJob)
+        if containsCycle:
+            return []
+    return orderedJob
+
+def dfs(node, orderedJob):
+    if node.visited:
+        return False
+    if node.visiting:
+        return True
+    node.visiting = True
+    for prereq in node.prereq:
+        containsCycle = dfs(prereq, orderedJob)
+        if containsCycle:
+            return True
+        
+    node.visiting = False
+    node.visited = True
+    orderedJob.append(node.job)
+    
+def createGraph(jobs, deps):
+    jobgraph = JobGraph(jobs)
+    for prereq, job in deps:
+        jobgraph.addPrereq(prereq, job)
+    return jobgraph
+
+class JobGraph:
+    def __init__(self, jobs):
+        self.nodes = []
+        self.graph = {}
+        for job in jobs:
+            self.addNode(job)
+            
+    def addNode(self, job):
+        self.graph[job] = JobNode(job)
+        self.nodes.append(self.graph[job])
+        
+    def addPrereq(self, prereq, job):
+        prereqNode = self.getNode(prereq)
+        jobNode = self.getNode(job)
+        jobNode.prereq.append(prereqNode)
+        
+    def getNode(self, job):
+        if job not in self.graph:
+            self.addNode(job)
+        return self.graph[job]
+        
+class JobNode:
+    def __init__(self, job):
+        self.job = job
+        self.prereq = []
+        self.visited = False
+        self.visiting = False
+           
+         
+  
