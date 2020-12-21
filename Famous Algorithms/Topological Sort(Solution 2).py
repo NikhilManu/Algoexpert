7da4e0113bab5f60cb29499d2654c75764f08a27
NikhilManu <70922592+NikhilManu@users.noreply.github.com>
2020-12-21 15:34:06 +0530

    Create Topological Sort(Solution 2).py

---
+++ Famous Algorithms/Topological Sort(Solution 2).py
@@ -0,0 +1,13 @@
+"""
+Course Schedule II [leetCode]
+"""
+"""
+There are a total of n courses you have to take labelled from 0 to n - 1.
+Some courses may have prerequisites, for example, if prerequisites[i] = [ai, bi] this means you must take the course ai before the course bi.
+Given the total number of courses numCourses and a list of the prerequisite pairs, return the ordering of courses you should take to finish all courses.
+If there are many valid answers, return any of them. If it is impossible to finish all courses, return an empty array.
+
+Input: numCourses = 4, prerequisites = [[1,0],[2,0],[3,1],[3,2]]
+Output: [3, 1, 2, 0]
+"""
+def TopSort(jobs, deps):
