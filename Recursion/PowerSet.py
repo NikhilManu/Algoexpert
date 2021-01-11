300337155d0831d822b57a300c83aa46a4cbb2a1
NikhilManu <70922592+NikhilManu@users.noreply.github.com>
2021-01-11 14:50:39 +0530

    Update PowerSet.py

--- Recursion/PowerSet.py
+++ Recursion/PowerSet.py
@@ -1,3 +1,14 @@
 """
 Subsets --> Name of this Qn in LeetCode
 """
+"""
+"""
+# 
+----------------
+def powerset(array):
+	subsets = [[]]
+	for num in array:
+		for i in range(len(subsets)):
+			curSub = subsets[i]
+			subsets.append(curSub + [num])
+	return subsets
