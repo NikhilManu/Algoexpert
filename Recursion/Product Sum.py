8a7ad9692975c5a3a76ce922cd0fbac6533d8035
NikhilManu <70922592+NikhilManu@users.noreply.github.com>
2021-01-11 13:43:54 +0530

    Update Product Sum.py

--- Recursion/Product Sum.py
+++ Recursion/Product Sum.py
@@ -15,11 +15,11 @@ is the sum of its elements, where "special" array insider it are summed themselv
 # time O(N) | Space O(d)
 -------------------------------
 def productSum(arr,depth = 1):
-	prod = 0
-	for i in arr:
-		if type(i) is list:
-			prod += depth * productSum(i, depth + 1)
-		else:
-			prod += depth * i
-	return prod
+   prod = 0
+   for i in arr:
+	if type(i) is list:
+	    prod += depth * productSum(i, depth + 1)
+	else:
+	    prod += depth * i
+   return prod
 
