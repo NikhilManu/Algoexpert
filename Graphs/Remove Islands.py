aa2beaa0da5a6e8b1ac25ab1c970439cd2416642
NikhilManu <70922592+NikhilManu@users.noreply.github.com>
2021-01-17 16:17:35 +0530

    Update Remove Islands.py

--- Graphs/Remove Islands.py
+++ Graphs/Remove Islands.py
@@ -3,6 +3,13 @@
 """
 
 """
+You're given a 2D array containing 0 and 1. An island is defined as any number of 1 which is horizontally
+or vertically adjacent but not diagonally adjacent
+It is not an island if it touches with the border of the matrix
+
+Note that island can be in T or L shaped.
+
+Write a program to remove all of the valid islands
 """
 
 # Solution
