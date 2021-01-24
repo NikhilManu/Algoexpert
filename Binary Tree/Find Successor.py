7705781d7ec19440b08c08597a698f4da966c4cf
NikhilManu <70922592+NikhilManu@users.noreply.github.com>
2021-01-25 03:51:11 +0530

    Create Find Successor.py

---
+++ Binary Tree/Find Successor.py
@@ -0,0 +1,32 @@
+"""
+Inorder Successor of BST
+"""
+
+"""
+Find the inorder Successor of a Binary Tree
+
+"""
+
+# Solution
+# O(h) Time | O(1) SPace
+------------------------
+def findSuccessor(tree, node):
+if node.right:
+return getLeftmostOfRight(node)
+
+return getRightmost(node)
+
+
+def getRightmost(node):
+cur = node
+while cur.parent and cur.parent.right == cur:
+cur = cur.parent
+
+return cur.parent
+
+def getLeftmostOfRight(node):
+cur = node.right
+while cur.left:
+cur = cur.left
+
+return cur
